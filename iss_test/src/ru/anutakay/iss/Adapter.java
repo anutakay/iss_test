6750f646e864cb862c823b14bb1725bac8a6f4eb
Anya Kaygorodova <anutakay@gmail.com>
2015-08-31 21:24:04 +0600

    Вариативность отображения.

--- iss_test/src/ru/anutakay/iss/Adapter.java
+++ iss_test/src/ru/anutakay/iss/Adapter.java
@@ -4,41 +4,48 @@ import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class Adapter extends SimpleAdapter {
     
     final static int LAYOUT = R.layout.item;
     
-    final static String[] FROM = { "track" };
+    final static String[] FROM = { "track", "filename" };
     
-    final static int[] TO = { R.id.text };
+    final static int[] TO = { R.id.text, R.id.text };
     
 
     public Adapter(Context context, List<Map<String, String>> data) {
         super(context, data, LAYOUT, FROM, TO);
     }
     
-    //TODO - перегрузить нужные функции для того, чтобы показывалось название если есть 
-
-   /* @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
+    @SuppressWarnings("unchecked")
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
-    }*/
+        View view = super.getView(position, convertView, parent);
+        TextView text = (TextView)view.findViewById(R.id.text);
+        
+        final Map<String, String> item = (Map<String, String>)getItem(position);
+        text.setText(item.get("filename"));
+        if (item.get("filename") != null) {
+            text.setText(item.get("filename"));
+        } else {
+            text.setText(item.get("track"));
+            
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
 
+                @Override
+                public void run() {
+                    item.put("filename", "\nololo\n");
+                    Adapter.this.notifyDataSetChanged();
+                }
+            }, 1000);
+        }
+        return view;
+    }
 }
