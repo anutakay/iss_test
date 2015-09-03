944d7f343bd9278fb62dfc144d6b0b6916c85af9
Anya Kaygorodova <anutakay@gmail.com>
2015-09-03 20:04:36 +0600

    Адаптер переделан на основе BaseAdapter.

--- iss_test/src/ru/anutakay/iss/Adapter.java
+++ iss_test/src/ru/anutakay/iss/Adapter.java
@@ -3,13 +3,15 @@ package ru.anutakay.iss;
 import java.util.List;
 import java.util.Map;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SimpleAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-public class Adapter extends SimpleAdapter {
+public class Adapter extends BaseAdapter {
     
     final static int LAYOUT = R.layout.item;
     
@@ -18,16 +20,30 @@ public class Adapter extends SimpleAdapter {
     final static int[] TO = { R.id.text, R.id.text };
     
     private Context context;
+    
+    List<Map<String, String>> data;
 
     public Adapter(Context context, List<Map<String, String>> data) {
-        super(context, data, LAYOUT, FROM, TO);
+        super();
         this.context = context;
+        this.data = data;
+    }
+    
+    @SuppressLint("ViewHolder")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        if(rowView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView  = inflater.inflate(LAYOUT, parent, false);
+        }   
+        rowView = this.bindView(rowView, position);      
+        return rowView;
     }
     
     @SuppressWarnings("unchecked")
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
+    private View bindView(View view, int position) {
         TextView text = (TextView)view.findViewById(R.id.text);
         
         final Map<String, String> item = (Map<String, String>)getItem(position);
@@ -41,5 +57,20 @@ public class Adapter extends SimpleAdapter {
             new Downloader(context).downloadIfMissing(url);
         }
         return view;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 }
